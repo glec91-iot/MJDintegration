b52e4da5ae1c113e4f62a23b693c573704161a78
glec91-iot <gleclercq.iot@gmail.com>
2016-04-30 23:38:41 +0200

    fin des mises à jour de BrancheJean pour avoir le même état que Majordomejean

--- MJDprg/SendMailPublicIP.py
+++ MJDprg/SendMailPublicIP.py
@@ -2,16 +2,16 @@ import smtplib
 from requests import get
 
 ip = get('http://api.ipify.org').text 
-to = 'gleclercq91@gmail.com'
-gmail_user = 'gleclercq91@gmail.com'
+to = 'yyyy@gmail.com'
+gmail_user = 'yyy@gmail.com'
 gmail_pwd ='xxxxxxx'
 smtpserver = smtplib.SMTP("smtp.gmail.com",587)
 smtpserver.ehlo()
 smtpserver.starttls()
 smtpserver.ehlo()
 smtpserver.login(gmail_user, gmail_pwd)
-header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:@pi \n'
-msg = header + '\n @pi='+ ip  +'\n\n'
+header = 'To:' + to + '\n' + 'From: ' + gmail_user + '\n' + 'Subject:@pp \n'
+msg = header + '\n @pp='+ ss  +'\n\n'
 print msg
 smtpserver.sendmail(gmail_user, to, msg)
 print 'done!'
